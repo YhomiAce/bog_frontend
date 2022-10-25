f1188d85cc991b1c98fe3c308d17dee4ab6f7056
Willscode <godswillijezei@gmail.com>
2022-10-25 17:36:00 +0100

    update homepage

---
+++ src/components/pages/home-comp/Faqs.js
@@ -0,0 +1,59 @@
+import { Fragment, useState } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+ 
+export default function Faqs() {
+  const [open, setOpen] = useState(1);
+ 
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+ 
+  return (
+    <Fragment>
+      <Accordion open={open === 1}>
+        <AccordionHeader onClick={() => handleOpen(1)}>
+            What is BOG for ?
+        </AccordionHeader>
+        <AccordionBody>
+          We're not always in the position that we want to be at. We're
+          constantly growing. We're constantly making mistakes. We're constantly
+          trying to express ourselves and actualize our dreams.
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 2}>
+        <AccordionHeader onClick={() => handleOpen(2)}>
+            How can I create an account ? 
+        </AccordionHeader>
+        <AccordionBody>
+          We're not always in the position that we want to be at. We're
+          constantly growing. We're constantly making mistakes. We're constantly
+          trying to express ourselves and actualize our dreams.
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 3}>
+        <AccordionHeader onClick={() => handleOpen(3)}>
+          What product can I buy on BOG ?
+        </AccordionHeader>
+        <AccordionBody>
+          We're not always in the position that we want to be at. We're
+          constantly growing. We're constantly making mistakes. We're constantly
+          trying to express ourselves and actualize our dreams.
+        </AccordionBody>
+      </Accordion>
+      <Accordion open={open === 4}>
+        <AccordionHeader onClick={() => handleOpen(4)}>
+        What is your return policy ?
+        </AccordionHeader>
+        <AccordionBody>
+          We're not always in the position that we want to be at. We're
+          constantly growing. We're constantly making mistakes. We're constantly
+          trying to express ourselves and actualize our dreams.
+        </AccordionBody>
+      </Accordion>
+    </Fragment>
+  );
+}
