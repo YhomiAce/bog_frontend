db75d7f257d7299182d5821f5f656f58e2f74da8
Willscode <godswillijezei@gmail.com>
2022-10-28 12:40:38 +0100

    merge lastname firstname

---
+++ src/components/layouts/assets/ProjectChart.js
@@ -0,0 +1,64 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+  
+export default function ProjectChart() {
+
+    const series = [
+        {
+          name: "Project",
+          data: [1, 2, 5, 3, 2, 1, 0, 3]
+        }
+      ];
+    
+    const guestOption = {
+        stroke: {
+            curve: "smooth",
+        },
+        xaxis: {
+            categories: ["Jan", "Feb", "Mar", "Apr","May", "Jun", "Jul", "Aug"]
+          }
+      };
+      return (
+        <div>
+          <ReactApexChart type="line" series={series} options={guestOption} width="100%" />
+        </div>
+      );
+}
+
+export function AdminChart() {
+
+  const series = [{
+    name: 'Clients',
+    data: [31, 40, 28, 51, 42, 55, 40, 34, 20, 10]
+  }, {
+    name: 'Service Partners',
+    data: [11, 32, 45, 32, 34, 52, 71, 24, 75, 76]
+  }, {
+    name: 'Product Partners',
+    data: [4, 8, 21, 8, 34, 32, 41, 37, 40, 50]
+  }];
+  const options = {
+    chart: {
+      height: 250,
+      type: 'area'
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'smooth',
+      width: 1,
+    },
+    xaxis: {
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
+    },
+    
+  };
+
+  return(
+    <div>
+      <ReactApexChart type="area" series={series} options={options} width="100%" />
+  </div>
+  )
+}
---
+++ src/components/layouts/assets/UsersChart.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+
+export default function UsersChart() {
+    
+    const options = { labels: ["Completed", "Ongoing", "Cancelled"],responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 320
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }] };
+const series = [20, 6, 3,]; //our data
+
+return (
+  <div className="">
+    <ReactApexChart options={options} series={series} type="donut" width="100%" />
+  </div>
+);
+  }
