3560f1cf67ab442b4835d971ecd87c10d1958c32
Willscode <godswillijezei@gmail.com>
2022-11-02 10:27:46 +0100

    settings kyc

--- src/components/Dashboard/clientDashboard/pages/Settings.js
+++ src/components/Dashboard/clientDashboard/pages/Settings.js
@@ -16,25 +16,25 @@ export default function Settings() {
         <div className="lg:grid-74">
           <div className="mt-6 py-6 bg-white px-4 rounded-lg">
             <Tabs>
-              <TabList>
+              <TabList className="fs-400 lg:fs-600 px-0 flex text-center">
                 <Tab>Personal Data</Tab>
                 <Tab>Change Password</Tab>
                 <Tab>Bank Details</Tab>
               </TabList>
               <TabPanel>
                 <div>
-                  <div>
-                    <div className="mt-6 lg:mt-0 py-6 px-8 rounded-lg">
+                  <div className="fs-400 lg:fs-600">
+                    <div className="mt-6 lg:mt-0 py-6 px-2 lg:px-8 rounded-lg">
                       <div className="flex items-center lg:my-6">
                         <div>
                             <img
                               src={require("../images/profile.png")}
                               alt="profifepic"
-                              className="lg:w-20 w-12"
+                              className="lg:w-20 w-16"
                             />
                         </div>
-                        <div className="fs-300 pl-6">
-                          <p>Update Profile Picture</p>
+                        <div className="lg:fs-400 text-xs pl-2 lg:pl-6 w-6/12">
+                          <p className="mb-2">Update Profile Picture</p>
                           <input type="file" accept="image" />
                         </div>
                       </div>
@@ -91,16 +91,16 @@ export default function Settings() {
                           </select>
                         </div>
                       </div>
-                      <div className="mt-10 flex justify-between items-center">
+                      <div className="mt-10 lg:flex justify-between items-center">
                         <button className="btn-primary lg:w-4/12">Update Profile</button>
-                        <p className="flex items-center fw-500 text-primary"><BiCheckCircle/><span className="pl-1">All Changes are saved</span></p>
+                        <p className="flex items-center fw-500 mt-4 lg:mt-0 text-primary"><BiCheckCircle/><span className="pl-1">All Changes are saved</span></p>
                       </div>
                     </div>
                   </div>
                 </div>
               </TabPanel>
               <TabPanel>
-                <div className="lg:w-11/12 lg:mt-6 mx-auto">
+                <div className="lg:w-11/12 lg:mt-6 mt-6 mx-auto">
                   <div className="pt-5">
                     <label className="block mb-1 fw-500">Old Password</label>
                     <input
@@ -125,16 +125,16 @@ export default function Settings() {
                   <div className="my-6 fs-400 lg:w-11/12 flex justify-between items-center">
                     <p className="flex items-center fw-500 text-primary"><BsInfoCircleFill className="text-xl text-primary"/><span className="pl-3 text-primary">Password should be minmum 8 letter and include lower and uppercase letter.</span></p>
                   </div>
-                  <div className="mt-10 lg:w-11/12 flex justify-between items-center">
+                  <div className="mt-10 lg:w-11/12 lg:flex justify-between items-center">
                     <button className="btn-primary">Update Password</button>
-                    <p className="flex items-center fw-500 text-primary"><BiCheckCircle/><span className="pl-1">All Changes are saved</span></p>
+                    <p className="flex items-center fw-500 text-primary mt-4 lg:mt-0"><BiCheckCircle/><span className="pl-1">All Changes are saved</span></p>
                   </div>
-                  <div className="mt-8 bg-light lg:px-5 px-2 py-4 rounded-lg">
-                    <p className="fw-600 lg:text-lg">Two-Factor Verification</p>
+                  <div className="mt-8 bg-light lg:px-5 px-3 py-4 rounded-lg">
+                    <p className="fw-600 fs-700 lg:text-lg">Two-Factor Verification</p>
                     <p className="fs-400 pt-5">Two-factor authentication is a method for protection your web account. When it is activated you need to enter not only your password, but also a special code. You can receive this code by in mobile app. Even if third person will find your password, then can't access with that code.</p>
-                    <div className="mt-10 lg:w-11/12 flex justify-between items-center">
+                    <div className="mt-10 lg:w-11/12 lg:flex justify-between items-center">
                       <button className="btn border border-blue-600 fs-500">Enable 2FA</button>
-                      <p className="flex items-center fs-300 fw-500 text-gray-500">CURRENT STATUS: <p>DISABLED</p></p>
+                      <p className="flex items-center fs-300 fw-500 text-gray-500 mt-4 lg:mt-0">CURRENT STATUS: <p>DISABLED</p></p>
                     </div>
                   </div>
                 </div>
@@ -170,23 +170,23 @@ export default function Settings() {
                       <option>Others</option>
                     </select>
                   </div>
-                  <div className="mt-10 lg:w-11/12 flex justify-between items-center">
+                  <div className="mt-10 lg:w-11/12 lg:flex justify-between items-center">
                     <button className="btn-primary">Update Bank Details</button>
-                    <p className="flex items-center fw-500 text-primary"><BiCheckCircle/><span className="pl-1">All Changes are saved</span></p>
+                    <p className="flex items-center fw-500 text-primary mt-4 lg:mt-0"><BiCheckCircle/><span className="pl-1">All Changes are saved</span></p>
                   </div>
                 </div>
               </TabPanel>
             </Tabs>
           </div>
           <div>
-            <div className="bg-white w-full py-6 lg:px-5 rounded-lg mt-6">
+            <div className="bg-white w-full py-6 lg:px-6 px-3 rounded-lg mt-6">
               <p className="fs-700 fw-600">Your Account Status</p>
               <div className="flex py-6">
                 <p className="px-4 py-2 bg-green-500 fw-600 fs-400 text-white rounded-lg">Email Verified</p>
                 <p className="px-4 py-2 bg-orange-500 fw-600 fs-400 text-white rounded-lg ml-5">KYC Pending</p>
               </div>
             </div>
-            <div className="bg-white w-full py-6 lg:px-5 rounded-lg mt-6">
+            <div className="bg-white w-full py-6 lg:px-6 px-3 rounded-lg mt-6">
               <p className="fs-700 fw-600">Identity Verification - KYC</p>
               <p className="mt-4">To comply with regulation, participant will have to go through indentity verification.</p>
               <p className="mb-6 mt-3">You have not submitted your KYC application to verify your indentity.</p>
