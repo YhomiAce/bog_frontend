ef427f7e30636b009993fa3dad58d9a81b92622a
Willscode <godswillijezei@gmail.com>
2022-11-24 13:48:23 +0100

    blog

--- src/components/Dashboard/clientDashboard/pages/Blog/EditPost.js
+++ src/components/Dashboard/clientDashboard/pages/Blog/EditPost.js
@@ -41,19 +41,19 @@ export default function EditPost() {
                     <div className="lg:grid-83 mb-8">
                         <div>
                             {/* blog post tittle */}
-                            <div className="lg:p-6 rounded-md bg-white">
+                            <div className="lg:p-6 p-3 pb-4  rounded-md bg-white">
                                 <p className="fw-600 lg:text-lg mb-3">Blog Post Tittle</p>
                                 <input type="text" value="Six reasons to use BOG" className="w-full px-3 rounded-md py-2 border border-gray-400" />
                             </div>
                             {/* blog post*/}
-                            <div className="lg:p-6 rounded-md bg-white mt-6">
+                            <div className="lg:p-6 p-3 pb-4  rounded-md bg-white mt-6">
                                 <p className="fw-600 lg:text-lg mb-3">Blog Post</p>
                                 <textarea value="Aliqua officia et aliqua eu magna cillum ea ipsum ullamco ipsum culpa ex qui. Officia aute minim excepteur fugiat fugiat est sunt sint incididunt ut minim. Pariatur magna est deserunt minim. Aliquip sunt adipisicing tempor veniam aliqua nisi ipsum mollit sit do. Commodo quis qui id anim minim aliqua aute sunt culpa." className="lg:h-56 h-28 w-full border border-gray-400 rounded-md px-2 py-4"></textarea>
                             </div>
-                            <div className="lg:p-6 rounded-md mt-6 bg-white">
+                            <div className="lg:p-6 p-3 pb-4  rounded-md mt-6 bg-white">
                                 <p className="fw-600 lg:text-lg pb-3 border-b border-gray-300">Add Photo(s)</p>
-                                <div className="lg:flex mt-4">
-                                    <div className="lg:w-3/12 border py-8 rounded-lg">
+                                <div className="flex mt-4">
+                                    <div className="lg:w-3/12 w-4/12 border py-8 rounded-lg">
                                         <input type="file" className="w-10/12 ml-2 text-sm" />
                                     </div>
                                     <div className="w-2/12 ml-4 rounded-lg">
@@ -67,7 +67,7 @@ export default function EditPost() {
                         </div>
                         <div>
                             {/* blog post category*/}
-                            <div className="lg:p-6 rounded-md bg-white">
+                            <div className="lg:p-6 mt-6 lg:mt-0 p-3 pb-4 rounded-md bg-white">
                                 <p className="fw-600 lg:text-lg pb-3 border-b border-gray-300">Comments</p>
                                 <div className="mt-4 fs-500">
                                     <div className="flex">
@@ -85,7 +85,7 @@ export default function EditPost() {
                                 </div>
                             </div>
                             {/* blog post category*/}
-                            <div className="lg:p-6 rounded-md bg-white mt-6">
+                            <div className="lg:p-6 p-3 pb-4  rounded-md bg-white mt-6">
                                 <p className="fw-600 lg:text-lg pb-3 border-b border-gray-300">Add Photo(s)</p>
                                 <div className="mt-4">
                                     <div className="flex">
@@ -110,8 +110,8 @@ export default function EditPost() {
                                     </div>
                                 </div>
                             </div>
-                            {/* blog post category*/}
-                            <div className="lg:p-6 rounded-md mt-6 bg-white">
+                            {/* blog post comments*/}
+                            <div className="lg:p-6 p-3 pb-4  rounded-md mt-6 bg-white">
                                 <p className="fw-600 lg:text-lg pb-3 border-b border-gray-300">Comments</p>
                                 <div className="mt-4">
                                     <div className="flex">
@@ -127,8 +127,8 @@ export default function EditPost() {
                         </div>
                     </div>  
                     <div className="border-t py-6 border-gray-400 text-end">
-                        <button className="lg:px-8 fw-500 py-2 border-pri text-primary mr-6 rounded-lg">Save To Draft</button>
-                        <button className="lg:px-8 fw-500 py-2 bg-primary text-white rounded-lg">Approve Product</button>
+                        <button className="lg:px-8 px-4 fw-500 py-2 border-pri text-primary mr-6 rounded-lg">Save To Draft</button>
+                        <button className="lg:px-8 px-4 fw-500 py-2 bg-primary text-white rounded-lg">Approve Product</button>
                     </div>                
                 </div>
             </div>
