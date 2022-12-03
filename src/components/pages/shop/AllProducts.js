a35091908abd51930910be9c25d662cc3d81b373
sulymon51 <sulymon51@users.noreply.github.com>
2022-12-04 00:22:48 +0100

    Create Order Implemented

--- src/components/pages/shop/AllProducts.js
+++ src/components/pages/shop/AllProducts.js
@@ -5,7 +5,7 @@ import ReactStars from "react-rating-stars-component";
 // import sand from "../../assets/images/sands.png";
 
 
-const baseURL = process.env.REACT_APP_IMAGE_URL;
+// const baseURL = process.env.REACT_APP_IMAGE_URL;
 
 const AllProducts = ({ products }) => {
 
@@ -21,7 +21,7 @@ const AllProducts = ({ products }) => {
                         <div className="shadow-md px-3 pb-6 pt-3 lg:mt-12 mt-6 rounded-md" key={item.id}>
 
                             <Link to={`/shop/${item.id}`}>
-                                <img src={`${baseURL}/${item.image}`} alt="products" className="w-full h-20 lg:h-40 mx-auto" />
+                                <img src={`${item.image}`} alt="products" className="w-full h-20 lg:h-40 mx-auto" />
                                 <p className="lg:fs-600 fs-300 fw-500  lg:pt-6 pt-2">{item.name}</p>
                                 <ReactStars
                                     edit={false}
