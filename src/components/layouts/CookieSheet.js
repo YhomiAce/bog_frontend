4829a5759f8012584a0c7b4299ebbdaf95ebdf93
Willscode <godswillijezei@gmail.com>
2022-12-22 12:40:23 +0100

    review

--- src/components/layouts/CookieSheet.js
+++ src/components/layouts/CookieSheet.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CookieConsent from "react-cookie-consent";
-import { Link } from 'react-router-dom';
 
 const CookieSheet = () => {
     const handleAcceptCookie = () => {
@@ -12,10 +11,10 @@ const CookieSheet = () => {
             enableDeclineButton
             onAccept={handleAcceptCookie}
             buttonText="Accept"
-            style={{ background: "#3F79AD" }}
-            buttonStyle={{ color: "#EC8B20", fontSize: "15px", padding: "5px 10px", borderRadius: "10px" }}
+            // style={{ background: "#3F79AD" }}
+            // buttonStyle={{ color: "#EC8B20", fontSize: "15px", padding: "5px 10px", borderRadius: "10px" }}
         >
-           We use cookies to offer you a better browsing experience and analyze site traffic. If you continue to use this site, you consent to our use of cookies. <Link to="/">Learn More</Link>
+           We use cookies to offer you a better browsing experience and analyze site traffic. If you continue to use this site, you consent to our use of cookies.
         </CookieConsent>
     )
 }
